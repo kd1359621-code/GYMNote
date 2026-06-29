86e91f6233ab077d6af72ffe6e2e790ec4d6229c
Et <kd1359621@st.kobedenshi.ac.jp>
2026-06-29 13:26:31 +0900

    test

--- api/advice.js
+++ api/advice.js
@@ -1,4 +1,8 @@
 export default async function handler(req, res) {
+  console.log("API実行");
+}
+
+export default async function handler(req, res) {
   if (req.method !== "POST") {
     return res.status(405).json({ error: "POST only" });
   }
